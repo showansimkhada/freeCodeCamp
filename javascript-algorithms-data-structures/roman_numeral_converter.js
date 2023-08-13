2ac2b7f4f30a608a2855d21f127ec14253c4dcb6
Showan <44385680+showansimkhada@users.noreply.github.com>
2023-08-13 14:16:33 +1200

    feat/roman_numeral_converter

---
+++ javascript-algorithms-data-structures/roman_numeral_converter.js
@@ -0,0 +1,15 @@
+function convertToRoman(num) {
+    let romNum = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    let arbNum = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    let x = num;
+    let str = ''
+    for (let i in arbNum) {
+        while (x >= arbNum[i]) {
+            x -= arbNum[i]
+            str += romNum[i]
+        }
+    }
+    return str.toUpperCase();
+}
+   
+convertToRoman(29);
