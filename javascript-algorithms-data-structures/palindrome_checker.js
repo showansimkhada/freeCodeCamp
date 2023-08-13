4284524787176c4855e6ed10316e94e725cea1b5
Showan <44385680+showansimkhada@users.noreply.github.com>
2023-08-13 13:48:10 +1200

    feat/palindrome_checker

---
+++ javascript-algorithms-data-structures/palindrome_checker.js
@@ -0,0 +1,24 @@
+function palindrome(str) {
+    let array = str.split('')
+    let barr = []
+    let regex = new RegExp("[a-zA-Z0-9]")
+    // select only digits and letter
+    for (let i in array ) {
+        if (array[i].match(regex)) {
+            barr.push(array[i].toLowerCase())
+        }
+    }
+    // joint the array letter to one
+    let a = barr.join('')
+
+    // reverse the array and join
+    let b = barr.reverse().join('');
+
+    if (a === b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+palindrome("My age is 0, 0 si ega ym.");
