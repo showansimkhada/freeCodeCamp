7023b3ea8f43d8d71bcfad709e13f40184311b53
Showan <showansimkhada@hotmail.com>
2024-08-12 20:13:52 +1200

    moved file metada microservice to BEDL

--- back-end-development-api/file_metadata_microservice/index.js
+++ back-end-development-api/file_metadata_microservice/index.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser')
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'})
 require('dotenv').config()
 
 var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+    })
+})
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log('Your app is listening on port ' + port)
 });
