84008cbe1a6c0608b77e3e8fdff3a608fd5af341
Showan <showansimkhada@hotmail.com>
2024-08-12 19:56:29 +1200

    modified model.js

--- back-end-development-api/exercise_tracker/model/model.js
+++ back-end-development-api/exercise_tracker/model/model.js
@@ -2,20 +2,9 @@ const { model, Schema } = require('mongoose');
 
 const userSchema = new Schema ({
     username: {type: String, required: true},
-    _id: String
+    log: {type: Array}
 });
 
 const USER = model("USER", userSchema);
 
-const exerciseSchema = new Schema({
-    _id: {type: String, required: true},
-    description: {type: String, required: true},
-    duration: {type: Number, required: true},
-    date: {type: Date},
-    userId: String
-});
-
-const ES = new model("Exercises", exerciseSchema);
-
 exports.USER = USER;
-exports.ES = ES;
