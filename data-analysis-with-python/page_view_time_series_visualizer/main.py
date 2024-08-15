b9282010e7be5babd8748c26d66b4850ebcd2002
Showan <showansimkhada@hotmail.com>
2024-08-15 14:19:14 +1200

    cloned from github

---
+++ data-analysis-with-python/page_view_time_series_visualizer/main.py
@@ -0,0 +1,8 @@
+# This entrypoint file to be used in development. Start by reading README.md
+import time_series_visualizer
+from unittest import main
+
+# Test your function by calling it here
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
---
+++ data-analysis-with-python/page_view_time_series_visualizer/time_series_visualizer.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+# Import data (Make sure to parse dates. Consider setting index column to 'date'.)
+df = None
+
+# Clean data
+df = None
+
+def draw_line_plot():
+  # Draw line plot
+  
+  return
+
+def draw_bar_plot():
+  # Copy and modify data for monthly bar plot
+  return
+
+def draw_box_plot():
+  # Prepare data for box plots (this part is done!)
+  return
