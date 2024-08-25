8190e75966ec92b08bd6091221dfe60834d98ba7
Showan Simkhada <showansimkhada@hotmail.com>
2024-08-26 11:54:31 +1200

    time changed to 1500

--- information-security/stock_price_checker/index.js
+++ information-security/stock_price_checker/index.js
@@ -63,7 +63,7 @@ const listener = app.listen(process.env.PORT || 3000, function () {
         console.log('Tests are not valid:');
         console.error(e);
       }
-    }, 3500);
+    }, 1500);
   }
 });
 
