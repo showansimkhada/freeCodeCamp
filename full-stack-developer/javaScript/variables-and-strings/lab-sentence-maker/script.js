b0cb045abf2505846d21433c1f39ddcb0d097533
Showan <showansimkhada@hotmail.com>
2025-10-06 20:26:38 +1300

    Completed javaScript build a sentence maker

---
+++ full-stack-developer/javaScript/variables-and-strings/lab-sentence-maker/script.js
@@ -0,0 +1,17 @@
+let adjective, noun, verb, place, adjective2, noun2;
+adjective = "wonderful";
+noun = "dragon";
+verb = "angry";
+place = "Mountain";
+adjective2 = "two";
+noun2 = "meat";
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". " + "The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+adjective = "wonderful";
+noun = "book";
+verb = "run";
+place = "Hamilton";
+adjective2 = "cold";
+noun2 = "park";
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". " + "The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+console.log("Second story: " + secondStory);
