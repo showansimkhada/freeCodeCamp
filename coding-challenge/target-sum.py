2a43c85cd770d554f28ad2b968c921390bd66d20
Showan <showansimkhada@hotmail.com>
2026-07-29 19:36:46 +1200

    Completed coding challenge target sum

---
+++ coding-challenge/target-sum.py
@@ -0,0 +1,29 @@
+################################## 17 August 2025 #####################################
+'''
+Targeted Sum
+Given an array of numbers and an integer target, find two unique numbers in the array that add up to the target value. 
+Return an array with the indices of those two numbers, or "Target not found" if no two numbers sum up to the target.
+
+The returned array should have the indices in ascending order.
+Tests:
+
+Passed: 1. find_target([2, 7, 11, 15], 9) should return [0, 1].
+Passed: 2. find_target([3, 2, 4, 5], 6) should return [1, 2].
+Passed: 3. find_target([1, 3, 5, 6, 7, 8], 15) should return [4, 5].
+Passed: 4. find_target([1, 3, 5, 7], 14) should return 'Target not found'.
+'''
+
+def find_target(arr, target):
+    result = []
+    for i in range(len(arr)-1):
+        if arr[i] + arr[i+1] == target:
+            result.append(i)
+            result.append(i+1)
+    if result:
+        return result
+    return 'Target not found'
+
+print(find_target([2, 7, 11, 15], 9))
+print(find_target([3, 2, 4, 5], 6))
+print(find_target([1, 3, 5, 6, 7, 8], 15))
+print(find_target([1, 3, 5, 7], 14))
